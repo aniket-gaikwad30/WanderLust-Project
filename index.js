623f7f91bb8918d62522433d1444bb187c2ac78f
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-01-19 16:26:56 +0530

    Added Boiler Plate and navbar and added some style

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const MONGU_URL = "mongodb://127.0.0.1:27017/WanderLust";
 const Listing = require("./Models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
-
+const ejsMate = require("ejs-mate");
+app.use(express.static(path.join(__dirname, "/public")));
 
 main()
   .then(() => {
@@ -25,7 +25,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-
+app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
   res.send("Responce");
@@ -66,6 +66,28 @@ app.get("/listings/:id", async (req, res) => {
 app.post("/listings", async (req, res) => {
   const newListing = new Listing(req.body.listing);
   await newListing.save();
+  res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", { listing });
+});
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+  res.redirect(`/listings/${id}`);
+});
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing);
   res.redirect("/listings");
 });
 
